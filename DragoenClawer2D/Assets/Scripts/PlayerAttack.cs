8a9b52371ac87ce0cbe50c0d55a0b2c65ba786b1
Алескей <manay.ru@mail.ru>
2026-08-31 12:36:44 +1000

    Анимация атаки игрока , скрипт атаки

---
+++ DragoenClawer2D/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public float attackRange = 1.5f;
+    public int damage = 10;
+
+    public Animator animator;
+
+    public SpriteRenderer spriteRenderer;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            PerformAttack();
+        }
+    }
+
+
+    private void PerformAttack()
+    {
+        animator.SetTrigger("Attack");
+        Vector2 attackDirection = spriteRenderer.flipX ? Vector2.left : Vector2.right;
+
+        Collider2D[] hitCollider = Physics2D.OverlapCircleAll(transform.position, attackRange);
+
+        foreach(Collider2D collider in hitCollider)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                Vector2 directionToEnemy = (collider.transform.position - transform.position).normalized;
+
+                if (Vector2.Dot(attackDirection, directionToEnemy) > 0)
+                {
+                    Debug.Log("Враг атакован!!!");
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
