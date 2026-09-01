b9d7ce72238308401c9c098c9ffb19963855177c
Алескей <manay.ru@mail.ru>
2026-09-01 13:08:08 +1000

    Камера следит за игроком

--- DragoenClawer2D/Assets/Scripts/CameraFallow.cs
+++ DragoenClawer2D/Assets/Scripts/CameraFallow.cs
@@ -4,14 +4,17 @@ public class CameraFallow : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset;
-    void Start()
-    {
-        if (target == null) return;
-        offset = transform.position - target.transform.position;
-    }
+    [SerializeField] private float timeOffset = 0.2f;
+
+    private Vector3 velocity;
+    //void Start()
+    //{
+    //    if (target == null) return;
+    //    offset = transform.position - target.transform.position;
+    //}
 
     void LateUpdate()
     {
-        transform.position = target.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, timeOffset);
     }
 }
